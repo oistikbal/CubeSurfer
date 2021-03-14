accd826784d51828045ad1c90db505c2b5440eaf
oistikbal <kralomer24@hotmail.com.tr>
2021-03-14 20:19:47 +0300

    Update CameraScript.cs

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -5,19 +5,21 @@ using UnityEngine;
 public class CameraScript : MonoBehaviour
 {
     GameObject m_player;
-    Vector3 m_cameraPos;
-    Vector3 m_initialCameraPosDiff;
+    GameObject m_hiddenObject; // created for simulate to transform and rotation of player;
 
 
     void Start()
     {
         m_player = GameObject.Find("player");
-        m_cameraPos = transform.position;
-        m_initialCameraPosDiff = m_cameraPos - m_player.transform.position;
+        m_hiddenObject = new GameObject();
+        m_hiddenObject.transform.position = transform.position;
+        m_hiddenObject.transform.rotation = transform.rotation;
+        m_hiddenObject.transform.parent = m_player.transform;
     }
 
     void FixedUpdate()
     {
-        transform.position = m_initialCameraPosDiff + new Vector3(m_player.transform.position.x, 0, m_player.transform.position.z);
+        transform.position = new Vector3(m_hiddenObject.transform.position.x, transform.position.y, m_hiddenObject.transform.position.z);
+        transform.rotation = m_hiddenObject.transform.rotation;
     }
 }
